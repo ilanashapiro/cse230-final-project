aa71e671cbf452d3ac38f573601f7fc972192bea
Ilana Shapiro <ishapiro00@gmail.com>
2023-12-03 20:32:23 -0800

    fix colors of text editor so background is clear, words now change color either as soon as you mistype (red) or after you press space and the word is correct (green) and words in progress remain black

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@
 {-# HLINT ignore "Eta reduce" #-}
 module Main where
 
+import Data.List (isPrefixOf)
+
 import Lens.Micro
 import Lens.Micro.Mtl (zoom)
 import Lens.Micro.TH ( makeLenses )
@@ -23,7 +25,7 @@ import Graphics.Vty (Key(KChar))
 
 data EditorName = EName deriving (Eq, Ord, Show)
 
-data State = ST { _editor :: E.Editor String EditorName, img :: V.Image, _wordIdx :: Int }
+data State = ST { _editor :: E.Editor String EditorName, img :: V.Image, _wordIdx :: Int, _numIncorrect :: Int, _lastCharIsSpace :: Bool }
 makeLenses ''State
 
 successAttrName :: A.AttrName
@@ -45,20 +47,25 @@ referenceText = "The sun dipped low on the horizon, casting a warm hue across th
                 \ to pause and embrace the tranquility of the natural world"
 
 -- Function to set word colors based on the comparison result
-coloredWordsWidget :: State -> String -> T.Widget EditorName
-coloredWordsWidget ts "" = C.str ""
-coloredWordsWidget ts str = foldl1 (C.<+>) $ map colorizeWord $ zip [0..] (words str)
+coloredWordsWidget :: Bool -> String -> T.Widget EditorName
+coloredWordsWidget _ "" = C.str ""
+coloredWordsWidget lastCharIsSpace str = foldl1 (C.<+>) $ map colorizeWord $ zip [0..] inputWords
     where
+        inputWords = words str
         colorizeWord (idx, word) =
             let referenceWord = words referenceText !! idx
-                attrName = if referenceWord == word then defaultAttrName else errorAttrName
+                attrName = 
+                    if not lastCharIsSpace && (idx == length inputWords - 1) && (isPrefixOf word referenceWord)
+                        then defaultAttrName
+                    else if referenceWord == word 
+                        then successAttrName 
+                    else errorAttrName
             in C.withAttr attrName $ C.str (word ++ " ")
 
 draw :: State -> [T.Widget EditorName]
 draw ts = [img' <=> e <=> wordCount] 
     where
-        e           = let customRender = coloredWordsWidget ts 
-                      in E.renderEditor (customRender . concat) True (ts ^. editor)
+        e           = E.renderEditor ((coloredWordsWidget (ts ^. lastCharIsSpace)) . concat) True (ts ^. editor)
         img'        = C.raw (img ts)
         wordCount   = C.strWrap $ "Word count: " ++ show (ts ^. wordIdx)
 
@@ -68,30 +75,32 @@ updateWordIdx ts =
       wordList = words $ concat text  -- Split text into words
   in ts & wordIdx .~ length wordList -- .~ is the lens operator for setting or updating the value viewed by the lens
 
-handleDefaultEvent :: T.BrickEvent EditorName e -> T.EventM EditorName State ()
-handleDefaultEvent e = do
-        zoom editor (E.handleEditorEvent e)
-        B.modify $ updateWordIdx
+handleDefaultEvent :: T.BrickEvent EditorName e -> Bool -> T.EventM EditorName State ()
+handleDefaultEvent e isLastCharSpace = do
+    zoom editor (E.handleEditorEvent e)
+    B.modify (\ts -> ts & lastCharIsSpace .~ isLastCharSpace)
+    B.modify updateWordIdx
 
 handleEvent :: T.BrickEvent EditorName e -> T.EventM EditorName State ()
-handleEvent (T.VtyEvent (V.EvKey (KChar 'c') [V.MCtrl]))   = B.halt
-handleEvent e@(T.VtyEvent (V.EvKey keyStroke _))           = 
+handleEvent (T.VtyEvent (V.EvKey (KChar 'c') [V.MCtrl])) = B.halt
+handleEvent e@(T.VtyEvent (V.EvKey (KChar ' ') _))       = do
+    handleDefaultEvent e True
+handleEvent e@(T.VtyEvent (V.EvKey keyStroke _))         = 
     let noOp = [V.KEnter, V.KBS, V.KLeft, V.KRight, V.KUp, V.KDown]
     in if elem keyStroke noOp
-        then return ()
-      else handleDefaultEvent e
-handleEvent e                                              = handleDefaultEvent e
+         then return ()
+       else handleDefaultEvent e False
+handleEvent e                                            = handleDefaultEvent e False
 
 initialState :: V.Image -> State
-initialState tImage = ST (E.editor EName (Just 1) "") tImage 0 -- (Just 1) means limit 1 line to the editor
+initialState tImage = ST (E.editor EName (Just 1) "") tImage 0 0 False -- (Just 1) means limit 1 line to the editor
 
 appAttrMap :: A.AttrMap
 appAttrMap = A.attrMap V.defAttr
-    [ 
-      (E.editAttr,      V.black `on` V.white)
-    , (successAttrName, V.green `on` V.white)
-    , (errorAttrName,   V.red `on` V.white)
-    , (defaultAttrName, V.black `on` V.white)
+    [ (E.editAttr,      V.withForeColor V.defAttr V.black)
+    , (successAttrName, V.withForeColor V.defAttr V.green)
+    , (errorAttrName,   V.withForeColor V.defAttr V.red)
+    , (defaultAttrName, V.withForeColor V.defAttr V.black)
     ]
 
 appCursor :: State -> [T.CursorLocation EditorName] -> Maybe (T.CursorLocation EditorName)

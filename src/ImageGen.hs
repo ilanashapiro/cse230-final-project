2e7d84d63419bb2166c6e489ab8cade3b7c6bf2d
rkthomps <rkthomps@calpoly.edu>
2023-12-09 16:47:44 -0800

    Random image revealing

--- src/ImageGen.hs
+++ src/ImageGen.hs
@@ -8,11 +8,13 @@ import System.FilePath.Posix
 import Control.Monad.Random 
 import Graphics.Vty as V
 import qualified Data.IMap as V
+import GHC.Profiling (stopHeapProfTimer)
 
 
 data Doc a = D [[a]] deriving (Show)
 
 data GrowthMethod   = LineByLineT2B
+                    | RandGrowth
                     deriving (Show)
 
 docFromPath :: FilePath -> IO (Doc Char)
@@ -27,8 +29,10 @@ defaultAttr = V.Attr V.Default V.Default V.Default V.Default
 
 data ImageDocs = IDocs {
     growth  :: GrowthMethod, 
-    doc :: Doc Char
+    doc :: Doc Char,
+    randMask :: [[Float]]
 } deriving (Show)
+
 
 maskRow :: [a] -> a -> [Bool] -> [a]
 maskRow [] _ _              = []
@@ -36,11 +40,13 @@ maskRow r  _ []             = r
 maskRow (el:els) def (m:ms) = maskedEl : maskRow els def ms
     where maskedEl = if m then el else def
 
+
 maskMatrix :: [[a]] -> a -> [[Bool]] -> [[a]]
 maskMatrix [] _ _             = [] 
 maskMatrix m  _ []            = m
 maskMatrix (r:rs) def (m:ms)  = maskedRow : maskMatrix rs def ms
     where maskedRow = maskRow r def m
+
 
 maskedImage :: Doc Char -> [[Bool]] -> V.Image 
 maskedImage (D cs) m = V.vertCat imageLines 
@@ -66,23 +72,50 @@ partialImgLBLT2B doc@(D lines) cur stop = maskedImage doc mask
         numLines = length lines * cur `div` stop
         mask     = firstNMask lines numLines 
 
+partialImageRand :: Doc Char -> [[Float]] -> Int -> Int -> V.Image
+partialImageRand doc@(D lines) floatMask cur stop = maskedImage doc mask
+    where 
+        f el    = el * fromIntegral stop <= fromIntegral cur
+        mask    = map (map f) floatMask
+
+
 partialImage :: ImageDocs -> Int -> Int -> V.Image
-partialImage (IDocs LineByLineT2B d) = partialImgLBLT2B d 
+partialImage (IDocs LineByLineT2B d _) = partialImgLBLT2B d 
+partialImage (IDocs RandGrowth d g) = partialImageRand d g
 
 
-getRandomGrowth :: (MonadRandom m) => m GrowthMethod
+getRandomGrowth :: IO GrowthMethod
 getRandomGrowth = do
     let growths     = [LineByLineT2B]
     idx             <- getRandomR (0, length growths - 1)
     return $ growths !! idx 
 
 
-getRandomImageDocs :: (MonadRandom m, MonadIO m) => String -> m ImageDocs
+getMatchingRandRow :: [a] -> IO [Float]
+getMatchingRandRow []       = return []
+getMatchingRandRow (el:els) = do
+    elVal   <- randomIO 
+    elVals  <- getMatchingRandRow els
+    return $ elVal : elVals 
+
+
+getMatchingRandMat :: [[Char]] -> IO [[Float]]
+getMatchingRandMat []       = return []
+getMatchingRandMat (r:rs)   = do 
+    randRow <- getMatchingRandRow r
+    randMat <- getMatchingRandMat rs
+    return $ randRow : randMat 
+
+
+getRandomImageDocs :: String -> IO ImageDocs
 getRandomImageDocs dirPath = do
     imgFiles    <- liftIO $ listDirectory dirPath
     idx         <- getRandomR (0, length imgFiles - 1)
     let path    = dirPath </> imgFiles !! idx 
-    doc         <- liftIO $ docFromPath path
-    growth      <- getRandomGrowth
-    return $ IDocs growth doc
+    doc@(D rs)  <- liftIO $ docFromPath path
+    randMask    <- getMatchingRandMat rs
+    --growth      <- getRandomGrowth
+    let growth  = RandGrowth
+    return $ IDocs growth doc randMask
+
 
